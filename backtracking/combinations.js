ea7b4960dc738d1db3d0be75107822b8beada723
Peter Reznick <preznick@gmail.com>
2018-01-16 12:14:29 -0500

    add solution

---
+++ backtracking/combinations.js
@@ -0,0 +1,26 @@
+// https://leetcode.com/problems/combinations/description/
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+var combine = function(n, k) {
+  const solutions = [];
+  helper(1, n, k, [], solutions);
+  return solutions;
+};
+
+function helper(start, end, length, solution, solutions) {
+  if (solution.length === length) {
+    solutions.push(solution.slice());
+  }
+  if (solution.length >= length) {
+    return;
+  }
+
+  for (let i = start; i <= end; i += 1) {
+    solution.push(i);
+    helper(i + 1, end, length, solution, solutions);
+    solution.pop();
+  }
+}
